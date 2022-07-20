a5f2f4c7c9018d9c01fe5b41dae5b12bd02b9ff3
Ankit Shubham <ankitshubham97@gmail.com>
2022-07-20 13:23:23 +0530

    fix: try converting class to npm module

--- sample/backend/index.js
+++ sample/backend/index.js
@@ -1,12 +1,48 @@
-import * as A from 'demo-authnft-js-server';
+import pkg from "demo-authnft-js-server";
+const { AuthNft } = pkg;
+console.log(new AuthNft());
 
-class App {
-  constructor() {
-    this.authnft = A.;
-  }
+// var authNft = new AuthNft({
+//     secret: "secret",
+//     networkEndpoint: "https://polygon-mumbai.infura.io/v3/f9179c2388e04d4ba17df0c8c6c13eae",
+//     deployedContractAbi: [
+//         {
+//           "inputs": [
+//             {
+//               "internalType": "address",
+//               "name": "_wallet",
+//               "type": "address"
+//             },
+//             {
+//               "internalType": "address",
+//               "name": "_contract",
+//               "type": "address"
+//             },
+//             {
+//               "internalType": "uint256",
+//               "name": "tokenId",
+//               "type": "uint256"
+//             }
+//           ],
+//           "name": "walletHoldsToken",
+//           "outputs": [
+//             {
+//               "internalType": "bool",
+//               "name": "",
+//               "type": "bool"
+//             }
+//           ],
+//           "stateMutability": "view",
+//           "type": "function"
+//         }
+//     ],
+//     deployedContractAddress: "0x3AC57D033895755e09F7e51BF42D2C4f47d988e6",
+// });
 
-  async start() {
-    await this.authnft.start();
-    this.authnft.
-  }
-}
+// var a = await authNft.getToken({nonce: "hello",
+// signature: "0x73ea35fb903e48406c49f01384a259d210eb443b4dec6f713c174fc9c870bc2424c5fd6423340b34769913bcd1e2b56c5e18f7ddc0803f76f6da1fb0b937fa931b",
+// walletPublicAddress: "0x4ad53d31Cb104Cf5f7622f1AF8Ed09C3ca980523",
+// nftContractAddress: "0x8437ee943b49945a7270109277942defe30fac25",
+// nftId: "0"})
+
+// console.log(a);
--- src/index.ts
+++ src/index.ts
@@ -6,37 +6,37 @@ import Web3 from 'web3';
 import { AbiItem } from 'web3-utils'
 import jwt from 'jsonwebtoken';
 
-export default class AuthNft {
+class AuthNft {
 
-  private static secret: string;
-  private static web3: Web3;
-  private static deployedContractAddress: string;
-  private static deployedContractAbi: AbiItem[];
+  private secret: string;
+  private web3: Web3;
+  private deployedContractAddress: string;
+  private deployedContractAbi: AbiItem[];
 
-  public static init({secret, networkEndpoint, deployedContractAddress, deployedContractAbi}: {secret: string, networkEndpoint: string, deployedContractAddress: string, deployedContractAbi: AbiItem[]}) {
-    AuthNft.secret = secret;
-    AuthNft.deployedContractAddress = deployedContractAddress;
-    AuthNft.deployedContractAbi = deployedContractAbi;
-    AuthNft.web3 = new Web3(networkEndpoint);
+  constructor({secret, networkEndpoint, deployedContractAddress, deployedContractAbi}: {secret: string, networkEndpoint: string, deployedContractAddress: string, deployedContractAbi: AbiItem[]}) {
+    this.secret = secret;
+    this.deployedContractAddress = deployedContractAddress;
+    this.deployedContractAbi = deployedContractAbi;
+    this.web3 = new Web3(networkEndpoint);
   }
 
-  private static createToken(getTokenRequest:GetTokenRequest): string {
+  private createToken(getTokenRequest:GetTokenRequest): string {
     const expiresIn = '1h';
     const { walletPublicAddress, nftContractAddress, nftId } = getTokenRequest;
     const jwtAccessTokenPayload: JwtAccessTokenPayload = {
       walletPublicAddress, nftContractAddress, nftId
     };
-    return jwt.sign(jwtAccessTokenPayload, AuthNft.secret, { expiresIn });
+    return jwt.sign(jwtAccessTokenPayload, this.secret, { expiresIn });
   } 
 
-  private static async doesWalletOwnNft(walletPublicAddress: string, nftContractAddress: string, nftId: string): Promise<boolean> {
+  private async doesWalletOwnNft(walletPublicAddress: string, nftContractAddress: string, nftId: string): Promise<boolean> {
     
     const contractInstance = new this.web3.eth.Contract(this.deployedContractAbi, this.deployedContractAddress);
     const res = await contractInstance.methods.walletHoldsToken(walletPublicAddress, nftContractAddress, nftId).call() as boolean;
     return res
   }
   
-  public static async getToken(getTokenRequest:GetTokenRequest): Promise<GetTokenResponse> {
+  public async getToken(getTokenRequest:GetTokenRequest): Promise<GetTokenResponse> {
     const { nonce, signature, walletPublicAddress, nftContractAddress, nftId } = getTokenRequest;
     try {
       // Verify the signature.
@@ -78,12 +78,14 @@ export default class AuthNft {
     }
   }
 
-  public static async verifyToken(token: string): Promise<boolean> {
+  public async verifyToken(token: string): Promise<boolean> {
     try {
-      jwt.verify(token, AuthNft.secret) as JwtAccessTokenPayload;
+      jwt.verify(token, this.secret) as JwtAccessTokenPayload;
       return true
     } catch (err) {
       return false
     }
   }
 }
+
+export default AuthNft;
